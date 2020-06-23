373475bb264467ea2b1deddd73b018c82abde8c8
qiang <zhouqiangk@gmail.com>
2020-06-23 08:01:41 -0700

    Update

--- app/src/main/java/com/laioffer/matrix/ControlPanel.java
+++ app/src/main/java/com/laioffer/matrix/ControlPanel.java
@@ -1,14 +1,109 @@
 package com.laioffer.matrix;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.text.DecimalFormat;
 
 public class ControlPanel extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_panel);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.baseline_home_black_18dp);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        // location tracker
+        final LocationTracker mLocationTracker = new LocationTracker(this);
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                // Response when the drawer's position changes
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+                final TextView user_textview = (TextView) drawerView.findViewById(R.id.user_name);
+                final TextView location_textview = (TextView) drawerView.findViewById(R.id.user_location);
+                // Respond when the drawer is opened
+                mLocationTracker.getLocation();
+                final double longtitute = mLocationTracker.getLongitute();
+                final double laitute = mLocationTracker.getLatitute();
+
+                if (Config.username == null) {
+                    user_textview.setText("");
+                    location_textview.setText("");
+                } else {
+                    user_textview.setText(Config.username);
+                    location_textview.setText("Lat = " + new DecimalFormat(".##").format(laitute) + ".Lon = " + new DecimalFormat(".##").format(longtitute));
+                }
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                // Respond when the drawer is closed
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                // Respond when the drawer motion state changes
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is lapped
+                        drawerLayout.closeDrawers();
+                        // update UI based on the item selected
+                        // swap UI fragment
+                        if (menuItem.getItemId() == R.id.drawer_layout) {
+                            Config.username = null;
+                            logout();
+                        }
+                        return true;
+                    }
+                }
+        );
     }
+
+    private void logout() {
+        finish();
+    }
+
 }
---
+++ app/src/main/java/com/laioffer/matrix/LocationTracker.java
@@ -0,0 +1,119 @@
+package com.laioffer.matrix;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class LocationTracker implements LocationListener {
+
+    private final Activity mContext;
+    private static final int PERMISSIONS_RESOURCE_LOCATION = 90;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60;
+
+    private boolean misGPSEnabled;
+    private boolean misNetworkEnabled;
+
+    private Location location;
+    private double latitute;
+    private double longitute;
+    private LocationManager locationManager;
+
+    public LocationTracker(Activity context) {
+        this.mContext = context;
+    }
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+            // getting GPS status
+            misGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            misNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!misGPSEnabled && !misNetworkEnabled) {
+                return null;
+            } else {
+                // first get location from network provider
+                checkLocationPermission();
+                if (misNetworkEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) this);
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitute = location.getLatitude();
+                            longitute = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled, get lat/long using GPS Service
+                if (misGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) this);
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitute = location.getLatitude();
+                                longitute = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    public double getLatitute() {
+        if (location != null) {
+            latitute = location.getLatitude();
+        }
+        return latitute;
+    }
+
+    public double getLongitute() {
+        if (location != null) {
+            longitute = location.getLongitude();
+        }
+        return longitute;
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // No explanation needed, we can request the permission
+            ActivityCompat.requestPermissions(mContext, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_RESOURCE_LOCATION);
+        }
+        return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
